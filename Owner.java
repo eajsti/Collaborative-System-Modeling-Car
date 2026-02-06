83e9af9e6a091d06502dfc248dc1691806618710
Justine <justinealamer0@gmail.com>
2026-02-06 12:10:11 +0800

    Added Class Owner
    Added Class Owner by Justine Alamer

---
+++ Owner.java
@@ -0,0 +1,21 @@
+public class Owner {
+    
+    public String name;
+
+    public Owner() {
+        this.name = "none";
+    }
+
+    public String identifyOwner() {
+
+            return "I am the Owner. I own the car. Owner name: " + name + ".";
+
+    }
+
+public void buyCar() {
+
+    
+}
+
+
+}
